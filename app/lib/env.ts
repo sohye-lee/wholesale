cf867fad772ef1da91f37c30cbf4afcc554b635e
Sohye Lee <sohyelee@gmail.com>
2024-04-12 17:31:28 -0400

    env

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -1,13 +1,11 @@
 export const env = {
-  NEXTAUTH_SECRET: "2c328062a7353e6386891997a8b49f78",
-  HOST: "http://localhost:3000",
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  HOST: process.env.HOST,
   // # NEXTAUTH_URL=http://localhost:3000,
   // # AUTH_URL=http://localhost:3000,
-  MONGODB_URL:
-    "mongodb+srv://sohye:Sohye8458@projects.gvqvc.mongodb.net/wholesale",
-  CLOUD_NAME: "depcdbduv",
-  CLOUD_API_KEY: "967491917998775",
-  CLOUD_API_SECRET: "PrVwkiysDeLu7kWqmav9SgHzo3E",
-  CLOUDINARY_URL:
-    "cloudinary://967491917998775:PrVwkiysDeLu7kWqmav9SgHzo3E@depcdbduv",
+  MONGODB_URL: process.env.MONGODB_URL,
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+  CLOUD_API_SECRET: process.env.CLOUD_API_SECRET,
+  CLOUDINARY_URL: process.env.CLOUDINARY_URL,
 };
